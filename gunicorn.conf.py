f23b7358953bfdeeaf4c16c7fd88a1ac7ec12911
Avinash Kumar <avinashbksc007@gmail.com>
2026-01-27 23:21:11 +0530

    Change worker class to UvicornWorker

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,6 @@ import multiprocessing
 bind = "0.0.0.0:10000"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 4
-worker_class = "gunicorn.workers.GunicornUvicornWorker"
+worker_class = "uvicorn.workers.UvicornWorker"  # Changed
 timeout = 120
 keepalive = 5
